920e3eeaa08080ce74e7026bcf636579d0bfb346
wxl <605933181@qq.com>
2021-10-22 13:14:15 +0800

    完成个人中心功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -54,7 +54,7 @@ function getUserInfo() {
 //渲染用户的头像
 function renderAvatar(user) {
     //1.获取用户的名称
-    var name = user.username || user.nickname;
+    var name = user.nickname || user.username;
     //2.设置欢迎的文本
     $('#welcome').html('欢迎&nbsp&nbsp' + name);
     //3.按需渲染用户头像
